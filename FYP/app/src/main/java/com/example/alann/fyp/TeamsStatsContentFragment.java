3bc5b3290e596757c7a9537d0ab88896964f7306
storm1996 <alannahsmullins@gmail.com>
2018-02-21 02:45:30 +0000

    New chart library with working example

--- FYP/app/src/main/java/com/example/alann/fyp/TeamsStatsContentFragment.java
+++ FYP/app/src/main/java/com/example/alann/fyp/TeamsStatsContentFragment.java
@@ -2,33 +2,28 @@ package com.example.alann.fyp;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.content.res.TypedArray;
-import android.graphics.Color;
-import android.graphics.DashPathEffect;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
+import com.github.mikephil.charting.charts.BarChart;
 
-import com.androidplot.util.PixelUtils;
-import com.androidplot.xy.CatmullRomInterpolator;
-import com.androidplot.xy.LineAndPointFormatter;
-import com.androidplot.xy.SimpleXYSeries;
-import com.androidplot.xy.XYGraphWidget;
-import com.androidplot.xy.XYPlot;
-import com.androidplot.xy.XYSeries;
 
-import java.text.FieldPosition;
-import java.text.Format;
-import java.text.ParsePosition;
-import java.util.Arrays;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 
 /**
  * Created by alann on 31/01/2018.
@@ -37,26 +32,44 @@ import java.util.Arrays;
 public class TeamsStatsContentFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
-                R.layout.recycler_view, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.recycler_view, container, false);
         ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
         recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         return recyclerView;
+
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView team_name;
-        public XYPlot plot;
+        public BarChart barChart;
+
         public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.item_teamstats, parent, false));
             team_name = (TextView) itemView.findViewById(R.id.page_title);
-            // initialize our XYPlot reference:
-            plot = (XYPlot) itemView.findViewById(R.id.plot);
+            barChart = (BarChart) itemView.findViewById(R.id.bar_chart);
+            barChart.animateY(800);
+            Description description = new Description();
+            description.setText("Points Scored");
+            barChart.setDescription(description);
+            // data has AxisDependency.LEFT
+            YAxis left = barChart.getAxisLeft();
+            //left.setDrawLabels(false); // no axis labels
+            //left.setDrawAxisLine(false); // no axis line
+            left.setDrawGridLines(false); // no grid lines
+            left.setDrawZeroLine(true); // draw a zero line
+            barChart.getAxisRight().setEnabled(false); // no right axis
+            barChart.moveViewToX(10);
 
+            // data has AxisDependency.LEFT
+            XAxis right = barChart.getXAxis();
+            right.setDrawLabels(false); // no axis labels
+            right.setDrawAxisLine(false); // no axis line
+            right.setDrawGridLines(false); // no grid lines
+            right.setAxisMinimum(0);
+            right.setSpaceMax(5f);
         //itemView.setOnClickListener(new View.OnClickListener() {
              // @Override
              // public void onClick(View v) {
@@ -71,41 +84,62 @@ public class TeamsStatsContentFragment extends Fragment {
 
     //Adapter to display recycler view.
 
-    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+    public class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
         // Set numbers of List in RecyclerView.
         // private static final int LENGTH = 18;
 
         private final String mTeam_name;
-        // create formatters to use for drawing a series using LineAndPointRenderer
-        // and configure them from xml:
-        LineAndPointFormatter series1Format;
-        private final Number[] xVals;
-        private final Number[] yVals;
-        public XYPlot plot;
-        public XYSeries series1;
+        BarDataSet set1;
+        BarDataSet set2;
+        BarData data;
+        ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
+        ArrayList<BarEntry> entries1 = new ArrayList<>();
+        ArrayList<BarEntry> entries2 = new ArrayList<>();
+        ArrayList<String> labels = new ArrayList<String>();
+        float groupSpace = 0.08f;
+        int startPos= 0;
+        float barWidth= 0.4f;
+        float barSpace = 0.03f; // x4 DataSet
+
         // private final Drawable[] mPlaceAvatars;
 
         public ContentAdapter(Context context) {
             Resources resources = context.getResources();
-            PixelUtils.init(context);
-            xVals = new Number[]{35, 73, 78, 88, 65, 68, 79, 77, 55, 53};
-            yVals = new Number[]{40, 82, 44, 20, 64, 61, 63, 55, 63, 72};
             mTeam_name = resources.getString(R.string.team_name);
+            entries1.add(new BarEntry(1f, 40));
+            entries1.add(new BarEntry(2f, 80));
+            entries1.add(new BarEntry(3f, 67));
+            entries1.add(new BarEntry(4f, 78));
+            entries1.add(new BarEntry(5f, 57));
+            entries1.add(new BarEntry(6f, 90));
+
+            set1 = new BarDataSet(entries1, "Home");
+
+            entries2.add(new BarEntry(1f, 35));
+            entries2.add(new BarEntry(2f, 65));
+            entries2.add(new BarEntry(3f, 70));
+            entries2.add(new BarEntry(4f, 50));
+            entries2.add(new BarEntry(5f, 54));
+            entries2.add(new BarEntry(6f, 75));
+
+            set2 = new BarDataSet(entries2, "Away");
+
+            //dataSets.add(set1);
+            //dataSets.add(set2);
+
+            //data = new BarData(dataSets);
+            data = new BarData(set1, set2);
+            set1.setColors(ColorTemplate.MATERIAL_COLORS);
+            set2.setColors(ColorTemplate.MATERIAL_COLORS);
+            data.groupBars(startPos, groupSpace, barSpace);
+            data.setBarWidth(barWidth);
             //TypedArray a = resources.obtainTypedArray(R.array.avatar);
             //mPlaceAvatars = new Drawable[a.length()];
             //for (int i = 0; i < mPlaceAvatars.length; i++) {
                 //mPlaceAvatars[i] = a.getDrawable(i);
             //}
             //a.recycle();
-            // just for fun, add some smoothing to the lines:
 
-            // turn the above arrays into XYSeries':
-            series1 = new SimpleXYSeries(Arrays.asList(xVals), Arrays.asList(yVals), mTeam_name);
-            series1Format = new LineAndPointFormatter(Color.RED, Color.GREEN, Color.BLUE, null);
-            series1Format.setInterpolationParams(
-                    new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
-            // add a new series' to the xyplot:
-            plot.addSeries(series1, series1Format);
         }
         @Override
         public TeamsStatsContentFragment.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -116,17 +150,7 @@ public class TeamsStatsContentFragment extends Fragment {
         public void onBindViewHolder(TeamsStatsContentFragment.ViewHolder holder, int position) {
             //holder.avatar.setImageDrawable(mPlaceAvatars[position % mPlaceAvatars.length]);
             holder.team_name.setText(mTeam_name);
-            plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
-                @Override
-                public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
-                    int i = Math.round(((Number) obj).floatValue());
-                    return toAppendTo.append(xVals[i]);
-                }
-                @Override
-                public Object parseObject(String source, ParsePosition pos) {
-                    return null;
-                }
-            });
+            holder.barChart.setData(data);
         }
 
         @Override
