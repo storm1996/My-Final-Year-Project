7b53b8563886dc2a00e6e710d80afd4610140271
storm1996 <alannahsmullins@gmail.com>
2018-03-03 02:18:50 +0000

    Opened new activity for selection page

--- FYP/app/src/main/java/com/example/alann/fyp/MatchRecordingSetup.java
+++ FYP/app/src/main/java/com/example/alann/fyp/MatchRecordingSetup.java
@@ -1,57 +1,34 @@
 package com.example.alann.fyp;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
 import android.support.graphics.drawable.VectorDrawableCompat;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Adapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
- * Created by alann on 23/01/2018.
+ * Created by alann on 02/03/2018.
  */
 
-public class MainMenu extends AppCompatActivity {
+public class MatchRecordingSetup extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_menu);
+        setContentView(R.layout.setup_matchrecording);
 
         // Adding Toolbar to Main screen
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitleTextColor(android.graphics.Color.WHITE);
-
-        // Setting ViewPager for each Tabs
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        setupViewPager(viewPager);
-
-        // Set Tabs inside Toolbar
-        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
 
         // Create Navigation drawer and inflate layout
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -80,62 +57,15 @@ public class MainMenu extends AppCompatActivity {
                         // Closing drawer on item click
                         mDrawerLayout.closeDrawers();
                         return true;
-                    }
+                   }
                 });
 
-        // Adding Floating Action Button to bottom right of main view
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                Context context = v.getContext();
-                Intent intent = new Intent(context, MatchRecordingSetup.class);
-                context.startActivity(intent);
-            }
-        });
-    }
-    private void setupViewPager(ViewPager viewPager) {
-        Adapter adapter = new Adapter(getSupportFragmentManager());
-        adapter.addFragment(new UpcomingMatchListContentFragment(), "Fixtures");
-        adapter.addFragment(new ResultsContentFragment(), "Results");
-        adapter.addFragment(new FunctionsContentFragment(), "Functions");
-        adapter.addFragment(new TeamsStatsContentFragment(), "Teams Statistics");
-        viewPager.setAdapter(adapter);
-    }
 
-    static class Adapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
-
-        public Adapter(FragmentManager manager) {
-            super(manager);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
-        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+         //Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -153,5 +83,5 @@ public class MainMenu extends AppCompatActivity {
             mDrawerLayout.openDrawer(GravityCompat.START);
         }
         return super.onOptionsItemSelected(item);
-    }
+   }
 }
--- statsHolder/models.py
+++ statsHolder/models.py
@@ -9,3 +9,42 @@ class statsHolder(models.Model):
 	gearNumber = models.IntegerField()
 	twoPointer = models.IntegerField()
 
+class User(models.Model):
+	user_id = models.AutoField(primary_key=True)
+	username = models.CharField(max_length=15, unique=True)
+	password = models.CharField(max_length=25, unique=True)
+
+class Team(models.Model):
+	team_id = models.AutoField(primary_key=True)
+	team_name = models.CharField(max_length=25)
+	league = models.CharField(max_length=25)
+	home_location = models.CharField(max_length=55)
+	user_id =
+
+class Fixture(models.Model):
+	fixture_id = models.AutoField(primary_key=True)
+	home_team =
+	away_team =
+	fixture_date = models.DateField()
+	venue = models.CharField(max_length=55)
+
+class Player(models.Model):
+	player_id = models.AutoField(primary_key=True)
+	player_name = models.CharField(max_length=25)
+	player_number = models.IntegerField()
+	team_id =
+
+class Action(models.Model):
+	action_id = models.AutoField(primary_key=True)
+	action_type = models.CharField(max_length=25)
+	action_timestamp = models.DateTimeField(auto_now_add=True)
+	player_id =
+	fixture_id =
+
+class Substitutions(models.Model):
+	sub_id = models.AutoField(primary_key=True)
+	sub_timestamp = models.DateTimeField(auto_now_add=True)
+	sub_in = models.BooleanField()
+	sub_out = models.BooleanField()
+	player_id =
+	fixture_id =
